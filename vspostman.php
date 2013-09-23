4022c34d1943a4f4add1fc8d619d6c58b686fae1
Vitaly Serov <serovvitaly@gmail.com>
2013-09-24 01:21:46 +0600

    первый коммит

---
+++ vspostman.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Plugin Name: VsPostman
+* Description: Почтовый рассыльщик
+* Version: 1.0.0
+* Author: Vitaly Serov
+* Author URI: serovvitaly@gmail.com
+*/
+
+add_action( 'admin_menu', 'vspostman_admin_menu' );
+
+function vspostman_admin_menu() {
+    add_menu_page('Почтовик', 'Почтовик', 'gogo', 'vspostman_admin', '', '', 7);
+    
+    add_submenu_page('vspostman_admin', 'Письма', 'Письма', 'manage_options', 'vspostman-mails', 'vspostman_menu_mails');
+    add_submenu_page('vspostman_admin', 'Клиенты', 'Клиенты', 'manage_options', 'vspostman-clients', 'vspostman_menu_clients');
+    add_submenu_page('vspostman_admin', 'Статистика', 'Статистика', 'manage_options', 'vspostman-stats', 'vspostman_menu_stats');
+}
+
+
+function vspostman_menu_mails() {
+    echo 'MAILS';
+}
+function vspostman_menu_clients() {
+    echo 'CLIENTS';
+}
+function vspostman_menu_stats() {
+    echo 'STATS';
+}
