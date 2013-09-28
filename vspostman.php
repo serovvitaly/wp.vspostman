4268e078d288085a4caa2ee511df40fe80e5d75f
Vitaly Serov <serovvitaly@gmail.com>
2013-09-28 09:18:25 +0600

    очередной коммит

--- vspostman.php
+++ vspostman.php
@@ -9,7 +9,7 @@
 
 add_action('admin_menu', 'vspostman_admin_menu');
 
-do_action('vspostman_go');
+
 
 function vspostman_admin_menu() {
     add_menu_page('Почтовик', 'Почтовик', '', 'vspostman_admin', '', '/wp-content/plugins/vspostman/img/mail-air.png');
@@ -22,28 +22,68 @@ function vspostman_admin_menu() {
 
 function vspostman_menu_mails() {
     
-    $act = isset($_GET['act']) ? $_GET['act'] : NULL;
+    global $wpdb;
+    
+    $act = isset($_REQUEST['act']) ? $_REQUEST['act'] : NULL;
+    $uid = isset($_REQUEST['uid']) ? $_REQUEST['uid'] : 0;
+    
+    $_table_funnels = $wpdb->prefix . 'vspostman_funnels';    
     
     switch ($act) {
         case 'add':
-            $_ = array(
-                'title' => 'Новая воронка'
-            );
+            
+            $item = new stdClass();
+            $item->title = 'Новая воронка';
             
             include  'templates/mails/funnel-form.php';
             break;
         case 'edit':
+            if ($uid < 1) {
+                //
+            }
+            $item = $wpdb->get_row("SELECT * FROM {$_table_funnels} WHERE id={$uid}");
+            
+            $item->title = $item->name;
+            
+            include  'templates/mails/funnel-form.php';
+            break;
+        case 'save':
+        
+            $data = array(
+                'name' => isset($_POST['name']) ? $_POST['name'] : '',
+                'updated' => date('Y-m-d H:i:s'),
+            );
+        
+            if ($uid > 0) {
+                $wpdb->update($_table_funnels, $data, array('id' => $uid));
+            } else {
+                $wpdb->insert($_table_funnels, $data);
+            }
+            $redirect_to = '/wp-admin/admin.php?page=vspostman-mails';
+            include  'templates/redirect.php';
+            break;
+        case 'duplicate':
+            if ($uid < 1) {
+                //
+            }
             //
             break;
+        case 'stat':
+            if ($uid < 1) {
+                //
+            }
+            //
+            break;
+        case 'delete':
+            if ($uid < 1) {
+                //
+            }
+            $wpdb->delete($_table_funnels, array('id' => $uid));
+            $redirect_to = '/wp-admin/admin.php?page=vspostman-mails';
+            include  'templates/redirect.php';
+            break;
         default:
-            $items = array(
-                array(
-                    'id' => 1,
-                    'title' => 'Первая воронка',
-                    'date' => '12/12/1223',
-                    'readers' => 12
-                )
-            );
+            $items = $wpdb->get_results("SELECT * FROM {$_table_funnels}");
             
             include  'templates/mails/index.php';
     }
