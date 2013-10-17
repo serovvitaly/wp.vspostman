2f0805f99e630b70eea35df74357bf5fd09f7391
Vitaly Serov <serovvitaly@gmail.com>
2013-10-17 15:26:28 +0600

    импорт из Google Drive

--- templates/clients/importservices.php
+++ templates/clients/importservices.php
@@ -1,3 +1,47 @@
+<script>
+var CLIENT_ID = '333945996610.apps.googleusercontent.com';
+var SCOPES = 'https://www.googleapis.com/auth/drive.readonly';
+
+function checkAuth(handler) {
+    gapi.auth.authorize(
+        {'client_id': CLIENT_ID, 'scope': SCOPES, 'trashed': false},
+        handler);
+}
+function importFromGoogleDrive(){
+    checkAuth(function(authResult){
+        if (authResult && !authResult.error) {
+            gapi.client.request({
+              path: 'drive/v2/files',
+              params: {
+                  maxResults: 1000,
+                  q: "mimeType = 'application/vnd.google-apps.spreadsheet'"
+                  //q: "mimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'"
+              },
+              callback: function(data){
+                  if (data.items && data.items.length > 0) {
+                      var content = '';
+                      for (var i = 0; i < data.items.length; i++) {
+                          var item = data.items[i];
+                          content += '<option value="'+item.id+'">'+item.title+'</option>';
+                      }
+                      $('#clients-importservices-results .files-list').html(content);
+                  } else {
+                      //
+                  }
+                  
+                  $('#clients-importservices-links').slideUp();
+                  $('#clients-importservices-results').slideDown();
+              }
+            });           
+        } else {
+            alert('Ошибка авторизации Google, обратитесь к администратору.');
+        }
+    });
+}
+
+</script>
+<script src="https://apis.google.com/js/client.js?onload=handleClientLoad"></script>
+
 <h2 class="nav-tab-wrapper">
   <a href="/wp-admin/admin.php?page=vspostman-clients&act=import" class="nav-tab">Копировать & вставить</a>
   <a href="/wp-admin/admin.php?page=vspostman-clients&act=importfile" class="nav-tab">Загрузить файл</a>
@@ -9,10 +53,16 @@
   <div style="vertical-align: top; display: inline-block; width: 200px; margin-right: 10px;">
     <strong style="vertical-align: top;">Импортировать контакты из аккаунта:</strong>
   </div>
-  <img src="/wp-content/plugins/vspostman/img/google-docs.png" alt="">
-  <img src="/wp-content/plugins/vspostman/img/google-contacts.png" alt="">
+  <div id="clients-importservices-links">
+    <img style="cursor: pointer;" onclick="importFromGoogleDrive();" src="/wp-content/plugins/vspostman/img/google-docs.png" alt="">
+    <img style="cursor: pointer;" onclick="importFromGoogleContacts();" src="/wp-content/plugins/vspostman/img/google-contacts.png" alt="">
+  </div>
   
-  <div style="padding: 10px 0 0 214px;">
+  
+  <div id="clients-importservices-results" style="padding: 10px 0 0 214px; display: none;">
+    <div style="margin: 0 0 30px;">
+      <select class="files-list"><option></option></select>
+    </div>
     <input type="submit" class="button button-primary button-large" value="Импорт контактов">
   </div>
 
